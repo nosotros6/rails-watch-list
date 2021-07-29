7750eebd75638b6f49ce9ec59905c7253b4768f9
pilar garcia <pilargarciaroyo@gmail.com>
2021-07-29 11:38:28 -0400

    rutas ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'bookmark/create'
   # get 'bookmark/show'
   # get 'bookmark/destroy'  
+  root to: 'lists#index'
   resources :lists, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
