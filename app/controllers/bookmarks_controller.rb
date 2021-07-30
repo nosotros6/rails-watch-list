689c4cbcea56b21d52e9d6b3b6c6a9da8d2d3e81
pilar garcia <pilargarciaroyo@gmail.com>
2021-07-30 12:50:27 -0400

    bookmark ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,43 +1,42 @@
 class BookmarksController < ApplicationController
-  # before_action :set_bookmark, only: [:show, :destroy]
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
   def index
     @bookmarks = Bookmark.all
   end
 
   def new
-   # @bookmark = Bookmark.new
+   @bookmark = Bookmark.new
   end
 
   def create
-    # @bookmark = Bookmark.new(bookmark_params)
-    # @bookmark = Bookmark.find(params[:bookmark_id])
-    # @review.bookmark = @bookmark
-    # @review.save
-    # redirect_to bookmark_path(@bookmark)
-
-    # if @bookmark.save
-    # redirect_to @bookmark, notice: 'Bookmark was successfully created.'
-    # else
-      # render :new
-    # end
-  end
-
-  def show
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Has creado tu nota en Bookmark con éxito.'
+    else
+      render :new
+    end
   end
 
   def destroy
-    # @bookmark.destroy
-    # redirect_to bookmarks_path
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
-  #private
+  private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_bookmark
-     # @bookmark = Bookmark.find(params[:id])
-    #end
+    def set_bookmark
+      @bookmark = Bookmark.find(params[:id])
+    end
+
+    def set_list
+      @list = List.find(params[:list_id])
+    end
 
     # Only allow a trusted parameter "white list" through.
-    # def bookmark_params
-     # params.require(:bookmark).permit(:name)
-    # end
+    def bookmark_params
+      params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # get 'bookmark/index'
-  # get 'bookmark/new'
-  # get 'bookmark/create'
-  # get 'bookmark/show'
-  # get 'bookmark/destroy'  
   root to: 'lists#index'
-  resources :lists, only: [:index, :new, :create, :show]
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new,:create]
+  end 
+  resources :bookmarks, only: [ :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
